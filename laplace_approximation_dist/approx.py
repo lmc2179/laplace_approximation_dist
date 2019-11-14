3e3daf4e6bbc469ddb0dbebf801ab380f7313335
lmc2179 <louiscialdella@gmail.com>
2019-11-14 00:46:40 -0600

    rewrote approximation using numdifftools Hessian approximation

--- laplace_approximation_dist/approx.py
+++ laplace_approximation_dist/approx.py
@@ -1,10 +1,14 @@
 import numpy as np
 from scipy.optimize import minimize
 from scipy.stats import multivariate_normal
+from numdifftools import Hessian
 
 def approx_dist(lnprob, theta_init):
 	neg_lnprob = lambda v: -lnprob(v)
 	result = minimize(neg_lnprob, theta_init)
-	x_max, hess_inv = result.x, result.hess_inv
-	dist = multivariate_normal(x_max, hess_inv)
+	# x_max, hess_inv = result.x, result.hess_inv
+	x_max = result.x
+	h = Hessian(lnprob)(x_max)
+	print(h)
+	dist = multivariate_normal(x_max, -np.linalg.inv(h))
 	return dist
--- laplace_approximation_dist/tests/approx_demo.py
+++ laplace_approximation_dist/tests/approx_demo.py
@@ -1,10 +1,23 @@
 import numpy as np
-from scipy.stats import norm, multivariate_normal
+from scipy.stats import norm, multivariate_normal, beta
+from matplotlib import pyplot as plt
+import seaborn as sns
 from laplace_approximation_dist.approx import approx_dist
 
-def test_fxn(x):
-	return x[0]**2+x[1]**2
+# x0 = np.array([1,2])
+# result = approx_dist(multivariate_normal(np.array([0, 0]), np.eye(2)).logpdf, x0)
+# print(result.cov)
 
-x0 = np.array([1,2])
-result = approx_dist(multivariate_normal(np.array([0, 0]), np.eye(2)).logpdf, x0)
-print(result.cov)
+# x0 = np.array([1])
+# true_dist = norm(0, 1)
+# result = approx_dist(true_dist.logpdf, x0)
+# sns.distplot(true_dist.rvs(5000))
+# sns.distplot(result.rvs(5000))
+# plt.show()
+
+x0 = np.array([0.5])
+true_dist = beta(15, 15)
+result = approx_dist(true_dist.logpdf, x0)
+sns.distplot(true_dist.rvs(5000))
+sns.distplot(result.rvs(5000))
+plt.show()
