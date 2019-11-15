df64694cb65075591416c26f5970b9dc1a90803c
lmc2179 <louiscialdella@gmail.com>
2019-11-14 22:17:42 -0600

    added linear regression demo

--- laplace_approximation_dist/approx.py
+++ laplace_approximation_dist/approx.py
@@ -3,10 +3,9 @@ from scipy.optimize import minimize
 from scipy.stats import multivariate_normal
 from numdifftools import Hessian
 
-def approx_dist(lnprob, theta_init):
+def approx_dist(lnprob, v_init):
 	neg_lnprob = lambda v: -lnprob(v)
-	result = minimize(neg_lnprob, theta_init)
-	# x_max, hess_inv = result.x, result.hess_inv
+	result = minimize(neg_lnprob, v_init)
 	x_max = result.x
 	h = Hessian(lnprob)(x_max)
 	print(h)
--- laplace_approximation_dist/tests/approx_demo.py
+++ laplace_approximation_dist/tests/approx_demo.py
@@ -3,6 +3,7 @@ from scipy.stats import norm, multivariate_normal, beta
 from matplotlib import pyplot as plt
 import seaborn as sns
 from laplace_approximation_dist.approx import approx_dist
+from functools import partial
 
 # x0 = np.array([1,2])
 # result = approx_dist(multivariate_normal(np.array([0, 0]), np.eye(2)).logpdf, x0)
@@ -15,9 +16,32 @@ from laplace_approximation_dist.approx import approx_dist
 # sns.distplot(result.rvs(5000))
 # plt.show()
 
-x0 = np.array([0.5])
-true_dist = beta(15, 15)
-result = approx_dist(true_dist.logpdf, x0)
-sns.distplot(true_dist.rvs(5000))
-sns.distplot(result.rvs(5000))
+# x0 = np.array([0.5])
+# true_dist = beta(10, 15)
+# result = approx_dist(true_dist.logpdf, x0)
+# sns.distplot(true_dist.rvs(5000))
+# sns.distplot(result.rvs(5000))
+# plt.show()
+
+# Beta binomial
+# x0 = np.array([1., 1.])
+
+# Linear regression
+def lin_regression_lnprob(v, x=None, y=None):
+	a, b, s = v
+	if s <= 0:
+		return -np.inf
+	y_hat = a + b*x
+	lp = np.sum(norm.logpdf(y, y_hat, s))
+	return lp
+
+x = np.linspace(0, 1, 1000)
+y = .1 - 10*x + np.random.normal(0, 1, 1000)
+L = partial(lin_regression_lnprob, x=x, y=y)
+	
+result = approx_dist(L, np.array([0, 0, 1]))
+samples = result.rvs(1000)
+sns.jointplot(samples[:,0], samples[:,1])
+plt.show()
+sns.distplot(samples[:,2])
 plt.show()
