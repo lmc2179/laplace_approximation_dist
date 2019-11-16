4acfffba704759981c6c5ac23ce106f05b056ca1
lmc2179 <louiscialdella@gmail.com>
2019-11-16 11:01:30 -0600

    tabs to spaces

--- laplace_approximation_dist/approx.py
+++ laplace_approximation_dist/approx.py
@@ -4,9 +4,17 @@ from scipy.stats import multivariate_normal
 from numdifftools import Hessian
 
 def approx_dist(lnprob, v_init):
-	neg_lnprob = lambda v: -lnprob(v)
-	result = minimize(neg_lnprob, v_init)
-	x_max = result.x
-	h = Hessian(lnprob)(x_max)
-	dist = multivariate_normal(x_max, -np.linalg.inv(h))
-	return dist
+    """
+    Construct a laplace approximation to the distribution with the given log-density.
+    Arguments:
+    -lnprob:
+    -v_init:
+    Returns:
+    -approximate distribution, a scipy.stats.multivariate_normal object
+    """
+    neg_lnprob = lambda v: -lnprob(v)
+    result = minimize(neg_lnprob, v_init)
+    x_max = result.x
+    h = Hessian(lnprob)(x_max)
+    dist = multivariate_normal(x_max, -np.linalg.inv(h))
+    return dist
